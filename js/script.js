1ce6195b23c99ce0c42e0afce10a9fc3dac49515
koinoniays <koinonia_ys@naver.com>
2024-01-11 21:43:40 +0900

    마우스오버/아웃 테스트

--- js/script.js
+++ js/script.js
@@ -1,29 +1,47 @@
 window.addEventListener("load", () => {
     // const tl = gsap.timeline();
     // tl.call(section1Ani);
-
     // 로딩이 완료되면 로딩 화면 숨기기
     const loadingContainer = document.querySelector("#loading-screen");
     loadingContainer.style.display = "none";
+    // 섹션-1 애니메이션 실행
     section1Ani();
 });
 // 섹션-1 애니
 let pText1Section1 = document.querySelectorAll(".section-1 > .text-1 > p");
 let spanText1Section1 = document.querySelectorAll(".section-1 > .text-1 > span > span");
 let pText2Section1 = document.querySelector(".section-1 .text-2 > p");
-let rotateImg = document.querySelectorAll(".img-container > .img");
+let rotateImg = document.querySelectorAll(".img-container > div");
 
 let tl = gsap.timeline();
 function section1Ani() {
-    // 텍스트 애니
-    tl.from(spanText1Section1,{ 
-        opacity:0, y:-10, duration: 0.5, 
+    tl.from(spanText1Section1, { // 텍스트 애니
+        opacity: 0, y: -10, duration: 0.5,
         yoyo: true, stagger: { from: 'center', each: 0.1 }
     })
-    .fromTo(pText1Section1,{ opacity:0, y:50 }, {
-        opacity: 1, y: 0, duration: 1, stagger: 0.2
-    },'-=0.8')
-    .fromTo(rotateImg, { opacity: 0, rotateY: 0 }, {
-        opacity: 1, rotateY: 180,  duration: 1, stagger: 0.5 }, "-=0.8")
-    .from(pText2Section1, { opacity: 0,  y:50, duration: 1 }, "-=0.5");
+        .fromTo(pText1Section1, { opacity: 0, y: 50 }, { // 텍스트 애니
+            opacity: 1, y: 0, duration: 1, stagger: 0.2
+        }, '-=0.8')
+        .fromTo(rotateImg, { opacity: 0, rotateY: 180 }, { // 이미지 애니
+            opacity: 1, rotateY: 0, duration: 1, stagger: 0.5, ease: Power2.easeInOut
+        }, "-=0.8")
+        .from(pText2Section1, { opacity: 0, y: 50, duration: 1 }, "-=0.5"); // 텍스트 애니
 };
+// 섹션-1 이미지 마우스 오버/아웃
+let imgs = document.querySelectorAll(".section-1 .img-container > .img");
+imgs.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+        const p = e.currentTarget.querySelector("p");
+        if (p) {
+            p.style.display = "block";
+            e.currentTarget.classList.add("show-bg");
+        }
+    });
+    item.addEventListener("mouseout", (e) => {
+        const p = e.currentTarget.querySelector("p");
+        if (p) {
+            p.style.display = "none";
+            e.currentTarget.classList.remove("show-bg");
+        }
+    });
+});

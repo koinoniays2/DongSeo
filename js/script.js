f2a64ba29b514738f2405a35949ad67df228eba7
koinoniays <koinonia_ys@naver.com>
2024-01-11 23:16:21 +0900

    section-2-banner-test

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ window.addEventListener("load", () => {
     loadingContainer.style.display = "none";
     // 섹션-1 애니메이션 실행
     section1Ani();
+    // 섹선-2 애니메이션 실행
+    section2Banner();
 });
 // 섹션-1 애니
 let pText1Section1 = document.querySelectorAll(".section-1 > .text-1 > p");
@@ -23,8 +25,8 @@ function section1Ani() {
             opacity: 1, y: 0, duration: 1, stagger: 0.2
         }, '-=0.8')
         .fromTo(rotateImg, { opacity: 0, rotateY: 180 }, { // 이미지 애니
-            opacity: 1, rotateY: 0, duration: 1, stagger: 0.5, ease: Power2.easeInOut
-        }, "-=0.8")
+            opacity: 1, rotateY: 0, duration: 1, stagger: 0.2, ease: Power2.easeInOut
+        }, "-=1")
         .from(pText2Section1, { opacity: 0, y: 50, duration: 1 }, "-=0.5"); // 텍스트 애니
 };
 // 섹션-1 이미지 마우스 오버/아웃
@@ -34,14 +36,29 @@ imgs.forEach((item) => {
         const p = e.currentTarget.querySelector("p");
         if (p) {
             p.style.display = "block";
-            e.currentTarget.classList.add("show-bg");
         }
+        e.currentTarget.classList.add("show-bg");
     });
     item.addEventListener("mouseout", (e) => {
         const p = e.currentTarget.querySelector("p");
         if (p) {
             p.style.display = "none";
-            e.currentTarget.classList.remove("show-bg");
         }
+        e.currentTarget.classList.remove("show-bg");
     });
 });
+// 섹션-2 배너 애니
+const section2 = document.querySelector(".section-2");
+gsap.registerPlugin(ScrollTrigger);
+const section2Banner = () => {
+    gsap.from(section2, {
+        width: 0,
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: section2,
+            start: "top+=100 bottom",
+            toggleActions: "restart"
+        }
+    });
+}

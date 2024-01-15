e607f54c3617abf9acacd569be69e2fb34d0acbc
koinoniays2 <koinonia_ys@naver.com>
2024-01-15 23:46:51 +0900

    코드정리

--- js/script.js
+++ js/script.js
@@ -4,31 +4,35 @@ window.addEventListener("load", () => {
     loadingContainer.style.display = "none";
 
     // 섹션-1 애니메이션 실행
-    section1Ani();
+    sectionAni1();
     // 섹션-2 애니메이션 실행
-    section2Ani();
+    sectionAni2()
     // Partners 애니메이션 실행
-    partnersAni();
+    sectionAni3();
 });
+
 gsap.registerPlugin(ScrollTrigger);
 // --------------------섹션-1--------------------
-const pText1Section1 = document.querySelectorAll(".section-1 > .text-container > p");
-const spanText1Section1 = document.querySelectorAll(".section-1 > .text-container > span > span");
-const pText2Section1 = document.querySelector(".section-1 .text-2 > p");
+const sectionTitleSpan1 = document.querySelectorAll(".section-1 > .title > span");
+const sectionTitleP1 = document.querySelectorAll(".section-1 > .title > p");
 const rotateImg = document.querySelectorAll(".img-container > div");
-function section1Ani() {
+const sectionDescription1 = document.querySelector(".section-1 .description > p");
+function sectionAni1() {
     const tl = gsap.timeline();
-    tl.from(spanText1Section1, { // span 애니
+    // 타이틀 애니
+    tl.from(sectionTitleSpan1, {
         opacity: 0, y: -10, duration: 0.5,
         yoyo: true, stagger: { from: 'center', each: 0.1 }
     })
-        .fromTo(pText1Section1, { opacity: 0, y: 50 }, { // p 애니
+        .fromTo(sectionTitleP1, { opacity: 0, y: 50 }, {
             opacity: 1, y: 0, duration: 1, stagger: 0.2
         }, '-=0.8')
-        .fromTo(rotateImg, { opacity: 0, rotateY: 180 }, { // 이미지 애니
+    // 이미지 애니(rotate)
+        .fromTo(rotateImg, { opacity: 0, rotateY: 180 }, {
             opacity: 1, rotateY: 0, duration: 1, stagger: 0.2, ease: Power2.easeInOut
         }, "-=1")
-        .from(pText2Section1, { opacity: 0, y: 50, duration: 1 }, "-=0.5"); // 텍스트 애니
+    // 내용 애니
+        .from(sectionDescription1, { opacity: 0, y: 50, duration: 1 }, "-=0.5"); // 텍스트 애니
     ScrollTrigger.create({
         trigger: ".section-1",
         start: "top center",
@@ -40,7 +44,7 @@ function section1Ani() {
     // ScrollTrigger 초기화
     ScrollTrigger.refresh();
 };
-// 섹션-1 이미지 마우스 오버/아웃
+// 이미지 마우스 오버/아웃
 const imgs = document.querySelectorAll(".section-1 .img-container > .img");
 imgs.forEach((item) => {
     item.addEventListener("mouseover", (e) => {
@@ -58,20 +62,23 @@ imgs.forEach((item) => {
         e.currentTarget.classList.remove("show-bg");
     });
 });
+
 // --------------------섹션-2--------------------
-const pText1Section2 = document.querySelectorAll(".section-2 > .text-container > p");
-const spanText1Section2 = document.querySelectorAll(".section-2 > .text-container > span > span");
-const bookImg = document.querySelector(".section-2-container");
-function section2Ani() {
+const sectionTitleSpan2 = document.querySelectorAll(".section-2 > .title > span");
+const sectionTitleP2 = document.querySelectorAll(".section-2 > .title > p");
+const bookImg = document.querySelector("#book-container");
+function sectionAni2() {
     const tl = gsap.timeline();
-    tl.from(spanText1Section2, { // span 애니
+    // 타이틀 애니
+    tl.from(sectionTitleSpan2, {
         opacity: 0, y: -10, duration: 0.5,
         stagger: { from: 'start', each: 0.1 }
     })
-        .fromTo(pText1Section2, { opacity: 0, y: 50 }, { // p 애니
+        .fromTo(sectionTitleP2, { opacity: 0, y: 50 }, {
             opacity: 1, y: 0, duration: 1, stagger: 0.2
         }, '-=1')
-        .from(bookImg, { x: 1000, duration: 1.5, ease: "power3.out" }, "-=1.3") // 책 애니
+    // 책 애니
+        .from(bookImg, { x: 1000, duration: 1.5, ease: "power3.out" }, "-=1") // 책 애니
     ScrollTrigger.create({
         trigger: ".section-2",
         start: "top center",
@@ -80,10 +87,12 @@ function section2Ani() {
     });
     ScrollTrigger.refresh();
 }
-const page1 = document.querySelector("#page-1");
-const pOn = document.querySelector(".section-2-container > .text-1 > p");
-page1.addEventListener("click", () => {
-    page1.classList.toggle("clicked");
+// 책 클릭 애니
+const page = document.querySelector("#page-1");
+const pOn = document.querySelector("#book-container > .book-text > p");
+page.addEventListener("click", () => {
+    // 책 클릭 시 펼쳐짐
+    page.classList.toggle("clicked");
     // 클릭 시 텍스트 on
     pOn.classList.toggle("on");
     if (pOn.classList.contains("on")) {
@@ -92,17 +101,24 @@ page1.addEventListener("click", () => {
         pOn.style.transform = "scale(0)";
     }
 });
+
 //--------------------섹션-3--------------------
 const partners = document.querySelector("#partners");
+const partnersB = document.querySelector("#partners-b-img");
 const partnersP = document.querySelector("#partners > p")
 const partnersImg = document.querySelectorAll("#partners > .logo-container > img");
-const section3Text = document.querySelectorAll(".section-3 > .text-container > p");
-function partnersAni() {
+const sectionDescription3 = document.querySelectorAll(".section-3 > .description > p");
+function sectionAni3() {
     const tl = gsap.timeline();
-    tl.from(partners, { opacity:0, x: -1000, duration: 0.5, }) // partners
-    .from(partnersP, { opacity:0, y: 20, duration: 0.5, }) // p 애니
-    .from(partnersImg, { opacity:0, scale: 0, stagger: 0.3, ease: "back.out", duration: 0.5 },"-=0.2") // 로고 애니
-    .from(section3Text, { opacity:0, y:50, duration:1, stagger:0.3 }, "-=0.3")
+    // partners 배경
+    tl.from(partners, { opacity:0, x: 1000, duration: 0.5, })
+    // 타이틀
+    .from(partnersP, { opacity:0, y: 20, duration: 0.5, })
+    // 로고 이미지
+    .from(partnersImg, { opacity:0, scale: 0, stagger: 0.3, ease: "back.out", duration: 0.5 },"-=0.2")
+    .from(sectionDescription3, { opacity:0, y:50, duration:1, stagger:0.3 }, "-=0.3")
+    // partners bottom 배경
+    .from(partnersB, { opacity:0, x: -1000, duration: 0.5, },"-=3")
     ScrollTrigger.create({
         trigger: ".section-3",
         start: "top center",
